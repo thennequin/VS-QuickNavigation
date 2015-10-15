7ba439d20213cc7e5801b5efc46f152bf8dab723
CameleonTH <cameleonth@gmail.com>
2015-10-15 21:55:40 +0200

    Add DteHelper class

---
+++ VS-QuickNavigation/Utils/DteHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_QuickNavigation.Utils
+{
+	public static class DteHelper
+	{
+		public static bool GetPropertyString(EnvDTE.Properties properties, string propertyName, out string sValueOut)
+		{
+			if (properties != null)
+			{
+				foreach (EnvDTE.Property item in properties)
+				{
+					if (item != null && item.Name == propertyName)
+					{
+						sValueOut = (string)item.Value;
+						return true;
+					}
+				}
+			}
+			sValueOut = null;
+			return false;
+		}
+
+		public static EnvDTE.Property GetProperty(EnvDTE.Properties properties, string propertyName)
+		{
+			if (properties != null)
+			{
+				foreach (EnvDTE.Property item in properties)
+				{
+					if (item != null && item.Name == propertyName)
+					{
+						return item;
+					}
+				}
+			}
+			return null;
+		}
+	}
+}
