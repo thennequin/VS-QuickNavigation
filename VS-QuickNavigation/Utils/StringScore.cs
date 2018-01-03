feb1b7c1744d52f477befcd490e8fe72d104677b
Thibault Hennequin <cameleonth@gmail.com>
2018-01-03 19:50:15 +0100

    Remove old search functions

--- VS-QuickNavigation/Utils/StringScore.cs
+++ VS-QuickNavigation/Utils/StringScore.cs
@@ -104,60 +104,6 @@ namespace VS_QuickNavigation
 			return totalScore;
 		}
 
-		/*void SearchBestToken(string query, string inText, int inTextStart, int queryStart, out int bestScore, out int startBest, out int len)
-		{
-			int tokenIndex = queryStart;
-			int stringIndex = inTextStart;
-			int totalScore = 0;
-			int combo = 1;
-			int? currentMatch = null;
-
-			//int bestScore = 0;
-			//int bestScoreLastCharPos = 0;
-
-			while (stringIndex < inText.Length)
-			{
-				
-
-				int charScore = CharScore(inText[stringIndex], query[tokenIndex]);
-				if (charScore > 0)
-				{
-					if (!currentMatch.HasValue)
-					{
-						currentMatch = stringIndex;
-					}
-
-					int multScore = stringIndex >= doubleScoreStart ? 2 : 1; // Double score
-					totalScore += charScore * combo * multScore;
-
-
-					//if (charScore == 2) //To test : ignore
-					{
-						++tokenIndex;
-						++combo;
-
-						if (tokenIndex >= query.Length)
-						{
-							++stringIndex;
-							break;
-						}
-					}
-				}
-				else
-				{
-					combo = 1;
-					if (null != matchIndexOut && currentMatch.HasValue)
-					{
-						Debug.Assert((stringIndex - currentMatch.Value) > 0);
-						matchIndexOut.Add(new Match(currentMatch.Value, stringIndex - currentMatch.Value));
-						currentMatch = null;
-					}
-				}
-
-				++stringIndex;
-			}
-		}*/
-
 		public static void FormatMatches(string sString, List<Match> matches, List<Tuple<string, bool>> formatted, int start = 0/*, int end = -1*/)
 		{
 			formatted.Clear();
@@ -189,67 +135,6 @@ namespace VS_QuickNavigation
 			{
 				formatted.Add(Tuple.Create(sString.Substring(start), false));
 			}
-		}
-
-		public static List<string> Search(
-			string search,
-			string words,
-			double fuzzyness)
-		{
-			IEnumerable<string> wordList = words.Split(
-				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-				'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-', '_');
-			return Search(search, wordList, fuzzyness);
-		}
-
-		public static List<string> Search(
-			string word,
-			IEnumerable<string> wordList,
-			double fuzzyness)
-		{
-			List<string> foundWords = new List<string>();
-
-			foreach (string s in wordList)
-			{
-				// Calculate the Levenshtein-distance:
-				int levenshteinDistance =
-					LevenshteinDistance(word, s);
-
-				// Length of the longer string:
-				int length = Math.Max(word.Length, s.Length);
-
-				// Calculate the score:
-				double score = 1.0 - (double)levenshteinDistance / length;
-
-				// Match?
-				if (score > fuzzyness)
-					foundWords.Add(s);
-			}
-			return foundWords;
-		}
-
-		public static int LevenshteinDistance(string a, string b)
-		{
-			if (String.IsNullOrEmpty(a) || String.IsNullOrEmpty(b)) return 0;
-
-			int lengthA = a.Length;
-			int lengthB = b.Length;
-			var distances = new int[lengthA + 1, lengthB + 1];
-			for (int i = 0; i <= lengthA; distances[i, 0] = i++) ;
-			for (int j = 0; j <= lengthB; distances[0, j] = j++) ;
-
-			for (int i = 1; i <= lengthA; i++)
-				for (int j = 1; j <= lengthB; j++)
-				{
-					int cost = b[j - 1] == a[i - 1] ? 0 : 1;
-					distances[i, j] = Math.Min
-						(
-						Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1),
-						distances[i - 1, j - 1] + cost
-						);
-				}
-			return distances[lengthA, lengthB];
 		}
 	}
 }
